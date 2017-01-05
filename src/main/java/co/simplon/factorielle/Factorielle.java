07a564a7992e87c6e2be94be63ec638663e5520f
Laurent <laurent.simplon@gmail.com>
2017-01-05 15:59:12 +0100

    Etape 4 : test OK

--- src/main/java/co/simplon/factorielle/Factorielle.java
+++ src/main/java/co/simplon/factorielle/Factorielle.java
@@ -4,20 +4,21 @@ package co.simplon.factorielle;
  * Factorielle
  */
 
+public class Factorielle {
+	public Factorielle() {
+		super();
+	}
 
-public class Factorielle 
-{
-    public Factorielle() {
-    		super();
-    }
-    
-    public long calculer(long n) {
-    		if (n > 1) {
-    			return n * calculer(n-1);
-    		} else if (n < 0) {
-    			throw new IllegalArgumentException();
-    		}
-    		return n;
-    }
+	public long calculer(long n) {
+		if (n == 0) {
+			return 1;
+		}
+		if (n > 1) {
+			return n * calculer(n - 1);
+		} else if (n < 0) {
+			throw new IllegalArgumentException();
+		}
+		return n;
+	}
 
 }
--- src/test/java/co/simplon/factorielle/FactorielleTest.java
+++ src/test/java/co/simplon/factorielle/FactorielleTest.java
@@ -8,35 +8,48 @@ public class FactorielleTest {
 
 	@Test
 	public void factorielle_de_1_renvoie_1() {
-		
-	Factorielle monfactorielle = new Factorielle();
-	long n= monfactorielle.calculer(1);
-	
-	assertEquals(1, n);
-	
+
+		Factorielle monfactorielle = new Factorielle();
+		long n = monfactorielle.calculer(1);
+
+		assertEquals(1, n);
+
 	}
 
 	@Test
 	public void factorielle_de_3_renvoie_6() {
-		
-	Factorielle monfactorielle = new Factorielle();
-	long n= monfactorielle.calculer(3);
-	
-	assertEquals(6, n);
-	
+
+		Factorielle monfactorielle = new Factorielle();
+		long n = monfactorielle.calculer(3);
+
+		assertEquals(6, n);
+
 	}
-	
-	
+
 	@Test
 	public void factorielle_O_renvoie_1() {
-	
-	long n = 0;
-		
-	Factorielle monfactorielle = new Factorielle();
-	monfactorielle.calculer(0);
-	
-	assertEquals(1, n);
-	
+
+		long n = 0;
+
+		Factorielle monfactorielle = new Factorielle();
+		long result = monfactorielle.calculer(n);
+
+		assertEquals(1, result);
+
 	}
-	
+
+	/*
+	 * @Test public void
+	 * factorielle_nb_negatif_renvoie_IllegalArgumentException(){ long n=-1;
+	 * Factorielle monfactorielle =new Factorielle();
+	 * 
+	 * if (n<0){ throw new
+	 * IllegalArgumentException("Nombre negatif, impossible a résoudre"); } else
+	 * monfactorielle.calculer(n);
+	 * 
+	 * assertFalse("IllegalArgumentException", true);
+	 * 
+	 * }
+	 */
+
 }
